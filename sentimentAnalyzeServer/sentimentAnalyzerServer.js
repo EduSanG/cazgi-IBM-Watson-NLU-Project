f79bece276192fe4ca169830ab34c634af36f6c5
Eduardo <eduardo.sand.g@gmail.com>
2021-06-09 04:00:03 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv')
+dotenv.config();
+
+function getNLUInstance(){
+    const api_key = process.env.API_KEY;
+    const api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key
+        }),
+        serviceUrl: api_url
+    })
+    return naturalLanguageUnderstanding
+}
 
 app.use(express.static('client'))
 
@@ -11,20 +30,51 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+    let analyzeParams = {
+        'url': req.query.url,
+        'features': {
+            'emotion': {}
+        }
+    }
+    getNLUInstance().analyze(analyzeParams)
+    .then(response => res.send(response.result.emotion.document.emotion))
+    .catch(err => res.send("Error, Unable to reach"))
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    let analyzeParams = {
+        'url': req.query.url,
+        'features': {
+            'sentiment': {}
+        }
+    }
+    getNLUInstance().analyze(analyzeParams)
+    .then(response => res.send(response.result.sentiment.document.label))
+    .catch(err => res.send("Error, Unable to reach"))
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    let analyzeParams = {
+        'text': req.query.text,
+        'features': {
+            'emotion': {}
+        }
+    }
+    getNLUInstance().analyze(analyzeParams)
+    .then(response => res.send(response.result.emotion.document.emotion))
+    .catch(err => res.send("Error, Unable to reach"))
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    let analyzeParams = {
+        'text': req.query.text,
+        'features': {
+            'sentiment': {}
+        }
+    }
+    getNLUInstance().analyze(analyzeParams)
+    .then(response => res.send(response.result.sentiment.document.label))
+    .catch(err => res.send("Error, Unable to reach"))
 });
 
 let server = app.listen(8080, () => {
